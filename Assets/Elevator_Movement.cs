f1e281e41cbfc57d5bd1c8b9d08b95db344d884f
Jake Haas <haasjac@gmail.com>
2016-02-22 13:13:29 -0500

    Elevator

---
+++ Assets/Elevator_Movement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator_Movement : MonoBehaviour {
+
+    public bool up = true;
+    public float distance = 2f;
+    public float speed = 0.1f;
+
+    Vector3 start;
+    Vector3 target;
+    float startTime;
+    float journeyLength;
+    bool transitioning;
+
+
+	// Use this for initialization
+	void Start () {
+        transitioning = false;
+        start = transform.position;
+        if (up) {
+            target = start + distance * Vector3.up;
+        } else {
+            target = start + distance * Vector3.down;
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transitioning) {
+            Vector3 pos = transform.position;
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            pos.y = Mathf.Lerp(start.y, target.y, fracJourney);
+            transform.position = pos;
+            if (pos == target) {
+                transitioning = false;
+                pos = start;
+                start = target;
+                target = pos;
+            }
+        }
+
+    }
+
+    void OnMouseDown() {
+        if (Time.timeScale != 0) {
+            transitioning = true;
+            startTime = Time.time;
+            journeyLength = Mathf.Abs(start.y - target.y);
+        }
+    }
+}
--- Assets/Game_Controller.cs
+++ Assets/Game_Controller.cs
@@ -6,13 +6,16 @@ public class Game_Controller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        Time.timeScale = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if(Input.GetKeyDown(KeyCode.Escape)) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            Time.timeScale = 1;
         }
 	}
 }
