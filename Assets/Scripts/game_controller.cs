b74ad493c3c0775ab5df241522c52a22c55df40d
Jake Haas <haasjac@gmail.com>
2016-02-24 23:21:34 -0500

    teleporter and win screen

---
+++ Assets/Scripts/game_controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class game_controller : MonoBehaviour {
+
+    public List<GameObject> players;
+    public GameObject win_screen;
+
+    bool win;
+
+
+	// Use this for initialization
+	void Start () {
+        win_screen.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        win = true;
+	    foreach (GameObject go in players) {
+            if (!go.GetComponent<player>().pause)
+                win = false;
+        }
+        if (win) {
+            win_screen.SetActive(true);
+        }
+	}
+
+    public void playAgain() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void nextLevel() {
+        print("next level");
+    }
+}
---
+++ Assets/Scripts/teleporter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleporter : MonoBehaviour {
+
+    public GameObject other_portal;
+    [HideInInspector]
+    public bool can_switch = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll) {
+        if (can_switch && coll.gameObject.tag == "player") {
+            other_portal.GetComponent<teleporter>().can_switch = false;
+            coll.gameObject.transform.position = other_portal.transform.position;
+        }
+    }
+
+    void OnCollisionExit(Collision coll) {
+        if (coll.gameObject.tag == "player") {
+            can_switch = true;
+        }
+    }
+}
